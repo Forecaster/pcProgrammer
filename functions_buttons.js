807e7abceb0580defccd50fc38fa49cdeb9c23cb
Forecaster <forecaster71@gmail.com>
2015-10-05 09:48:22 +0200

    Fix typo

--- functions_buttons.js
+++ functions_buttons.js
@@ -6,7 +6,7 @@ function loadUrl()
   else if (elements[2].value != "")
     parseProgramFromJson(elements[2].value);
   else
-    addMessage("Enter a url or pase a program into the box for loading!");
+    addMessage("Enter a url or parse a program into the box for loading!");
   //elements[0].value = "";
   menuExit();
 }
